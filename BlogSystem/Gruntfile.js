da574eb1ebffd35aa07a5ff9dbf48b572e823283
lostm1nd <kliomba@abv.bg>
2014-10-10 01:15:42 +0300

    add grunfile to compile jade and start the server

---
+++ BlogSystem/Gruntfile.js
@@ -0,0 +1,56 @@
+/* global module, require */
+'use strict';
+
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    jade: {
+      compile: {
+        options: {
+          pretty: true
+        },
+        files: [{
+          expand: true,
+          src: 'public/**/*.jade',
+          ext: '.html'
+        }]
+      }
+    },
+    express: {
+      dev: {
+        options: {
+          script: './server.js'
+        }
+      }
+    },
+    watch: {
+      express: {
+        options: {
+          spawn: false
+        },
+        files:  [ './app/**/*.js' ],
+        tasks:  [ 'express:dev' ]
+      },
+      jade: {
+        options: {
+          livereload: true
+        },
+        files: ['./public/**/*.jade'],
+        tasks: ['jade']
+      },
+      frontend: {
+        options: {
+          livereload: true
+        },
+        files: [
+          './public/css/*.css',
+          './public/**/*.html',
+          './public/js/**/*.js'
+        ]
+      }
+    },
+  });
+
+  grunt.registerTask('serve', ['jade', 'express', 'watch']);
+};
